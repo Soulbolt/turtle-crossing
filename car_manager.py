3cbcae5c6618e45290cb85d6697e718052de737b
Soulbolt <soulbolt@gmail.com>
2023-12-16 11:09:55 -0700

    added code to display rectagle to represent a car with function to move to the left end of the screen on game start.

--- car_manager.py
+++ car_manager.py
@@ -10,4 +10,14 @@ class CarManager(Turtle):
     
     def __init__(self):
         super().__init__()
-        self.hideturtle()
+        self.shape("square")
+        self.shapesize(stretch_wid=2, stretch_len=1)
+        self.penup()
+        self.left(90)
+        # self.goto(-60, 0)
+
+    def move(self):
+        new_x = self.xcor() - MOVE_INCREMENT
+        for dist in range(10):
+            if self.xcor() > -320:
+                self.goto(new_x, self.ycor())
